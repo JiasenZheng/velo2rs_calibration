91d1176e9d3c47d06c4f6d5f4139a14de3f87e60
JiasenZheng <jiasenzheng2020@u.northwestern.edu>
2022-03-15 04:37:35 -0500

    complete find_point node

--- src/find_points.cpp
+++ src/find_points.cpp
@@ -5,15 +5,21 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+// Services
+#include "velo2rs/Position.h"
+#include "velo2rs/Record.h"
 
-static ros::Subscriber velo_cloud_sub;
-static ros::Subscriber rs_cloud_sub;
-static ros::Publisher velo_cloud_pub;
-static ros::Publisher rs_cloud_pub;
-// static double x_coord = 0.0;
+static ros::Subscriber cloud_sub;
+static ros::Publisher cloud_pub;
+static ros::ServiceServer set_pos;
+static ros::ServiceServer record_point;
 static double y_coord = 0.0;
 static double z_coord = 0.0;
 static double d = 0.3;
+
+static pcl::PointXYZ left_point, right_point;
+
+
 
 void velo_cloud_cb(const sensor_msgs::PointCloud2ConstPtr &input)
 {
@@ -23,6 +29,7 @@ void velo_cloud_cb(const sensor_msgs::PointCloud2ConstPtr &input)
     // Convert input senor sensor msg to pcl format
     pcl::PointCloud<pcl::PointXYZ> cloud_in;
     pcl::fromROSMsg(*input,cloud_in);
+
 
     for (unsigned int i = 0; i < cloud_in.size(); i++)
     {
@@ -39,6 +46,18 @@ void velo_cloud_cb(const sensor_msgs::PointCloud2ConstPtr &input)
                 colored_point.g = 0;
                 colored_point.b = 0;
                 colored_point.a = 255;
+                if (cloud_in[i].y > left_point.y)
+                {
+                    left_point.x = cloud_in[i].x;
+                    left_point.y = cloud_in[i].y;
+                    left_point.z = cloud_in[i].z;
+                }
+                if (cloud_in[i].y < right_point.y)
+                {
+                    right_point.x = cloud_in[i].x;
+                    right_point.y = cloud_in[i].y;
+                    right_point.z = cloud_in[i].z;
+                }
             }
             else
             {
@@ -59,7 +78,45 @@ void velo_cloud_cb(const sensor_msgs::PointCloud2ConstPtr &input)
     output.header = input->header;
 
     // Publishe the data
-    velo_cloud_pub.publish(output);
+    cloud_pub.publish(output);
+}
+
+bool set_pos_cb(velo2rs::Position::Request &req,
+                velo2rs::Position::Response &res)
+{
+    y_coord = req.y_coord;
+    z_coord = req.z_coord;
+    d = req.dis;
+
+    // Update the coordiante of left and right coordinates
+    left_point.y = y_coord;
+    left_point.z = z_coord;
+    right_point.y = y_coord;
+    right_point.z = z_coord;
+
+    return true;
+}
+
+bool record_point_cb(velo2rs::Record::Request &req,
+                velo2rs::Record::Response &res)
+{
+    if (req.side == "left")
+    {
+        res.point_x = left_point.x;
+        res.point_y = left_point.y;
+        res.point_z = left_point.z;
+    }
+    else if (req.side == "right")
+    {
+        res.point_x = right_point.x;
+        res.point_y = right_point.y;
+        res.point_z = right_point.z;
+    }
+    else
+    {
+        return false;
+    } 
+    return true;
 }
 
 int main(int argc, char** argv)
@@ -69,10 +126,18 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
 
     // Create subscribers, publishers, services
-    velo_cloud_sub = nh.subscribe("/mid/points", 1, velo_cloud_cb);
-    velo_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/velo/points",1);
-    
-    rs_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/rs/points",1);
+    cloud_sub = nh.subscribe("/mid/points", 1, velo_cloud_cb);
+    cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/velo/points",1);
+    set_pos = nh.advertiseService("/set_pos",set_pos_cb);
+    record_point = nh.advertiseService("/record_point",record_point_cb);
+
+    // Initialize points
+    left_point.x = 0.0;
+    left_point.y = y_coord;
+    left_point.z = z_coord;
+    right_point.x = 0.0;
+    right_point.y = y_coord;
+    right_point.z = z_coord;
 
     ros::Rate loop_rate(50);
     while(ros::ok())
